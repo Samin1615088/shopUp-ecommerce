d6fa196c8fbdaec64650fbb2035ab90e010e8c7a
Samin1615088 <samin.shafkat@gmail.com>
2021-04-06 11:42:51 +0600

    again dummy card component implemented using local-images full-responsive

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,14 +2,17 @@ import React from 'react';
 import Card from '../../subcomponents/Card/Card';
 
 const Home = () => {
+    const dummyItemData ={
+        imgUrl: 'https://dummyimage.com/900/000/fff&text=900x900',
+        name: 'dummyName',
+        price: '100'
+    }
     return (
         <>
             <h1>Home</h1>
-
-
             <div className="container">
                 <div className="row">
-                    <Card/>
+                    <Card itemData={dummyItemData}/>
                 </div>
             </div>
         </>
--- src/subcomponents/Card/Card.js
+++ src/subcomponents/Card/Card.js
@@ -2,17 +2,18 @@ import React from 'react';
 import localdummyImg from '../../products-images/image 0.png';
 
 
-const Card = () => {
+const Card = (props) => {
+    const {imgUrl, name, price} = props.itemData;
     return (
         <>
             <div className="col-12 col-md-4 my-3 my-md-4">
                 <div className="card h-100">
-                    <img src={localdummyImg} className="card-img-top" alt="..." />
+                    <img src={imgUrl} className="card-img-top" alt="..." />
                     <div className="card-body">
-                        <h5 className="card-title">Product Name</h5>
+                        <h5 className="card-title">{name}</h5>
                     </div>
                     <div className="card-footer d-flex justify-content-between">
-                        <h5 className="my-auto">Price: $234</h5>
+                        <h5 className="my-auto">{`Price: ${price}`}</h5>
                         <button type="button" className="btn btn-success">Buy Now</button>
                     </div>
                 </div>
