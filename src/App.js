e1e5a60cd7fd17e94e48d029759690dfcf5c826a
Samin1615088 <samin.shafkat@gmail.com>
2021-04-06 09:58:09 +0600

    router without private route implemented and 404 component added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,50 @@
-import './App.css';
+import Home from './components/Home/Home';
+import Login from './components/Login/Login';
+import Admin from './components/Admin/Admin';
+import Checkout from './components/Checkout/Checkout';
+import Orders from './components/Orders/Orders';
+import Header from './components/Header/Header';
+import NoMatch from './components/NoMatch/NoMatch';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div>
+    <Router>
+      <div>
+        <Header />
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/home">
+            <Home />
+          </Route>
 
-    </div>
+          <Route path="/orders">
+            <Orders />
+          </Route>
+
+          <Route path="/admin">
+            <Admin />
+          </Route>
+          
+          <Route path="/login">
+            <Login />
+          </Route>
+          
+          <Route path="/checkout">
+            <Checkout/>
+          </Route>
+
+          <Route exact path="/">
+            <Home />
+          </Route>
+
+          <Route path="*">
+            <NoMatch />
+          </Route>
+        </Switch>
+      </div>
+    </Router >
   );
 }
 
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <div>
-            <h1>Header</h1>
-        </div>
+        <>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/orders">Orders</Link>
+                    </li>
+                    <li>
+                        <Link to="/admin">Admin</Link>
+                    </li>
+                </ul>
+                <Link to="/login">
+                <button>Login</button>
+                </Link>
+            </nav>
+        </>
     );
 };
 
