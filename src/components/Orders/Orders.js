9895b4482912964c2c99f326eb744d75f5fbf149
Samin1615088 <samin.shafkat@gmail.com>
2021-04-07 15:50:51 +0600

    order page half done

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,9 +1,42 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { UserContext } from '../../App';
 
 const Orders = () => {
+    const [userStatus, setUserStatus] = useContext(UserContext);
+    const [products, setProducts] = useState([]);
+
+    // useEffect(() => {
+    //     fetch(`http://localhost:5050/allproducts`)
+    //         .then(response => response.json())
+    //         .then(data => console.log(data))
+    // }, []);
+
+    console.log(products);
+
     return (
-        <div>
+        <div className="container">
             <h1>Orders</h1>
+
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th scope="col">Product Name</th>
+                        <th scope="col">Weight</th>
+                        <th scope="col">Quantity</th>
+                        <th scope="col">Price</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        <tr>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                            <td>120</td>
+                        </tr>
+                    }
+                </tbody>
+            </table>
         </div>
     );
 };
