e61fc287228527f19e261db7b3f588cb38a74120
Samin1615088 <samin.shafkat@gmail.com>
2021-04-07 09:37:19 +0600

    basic admin panel is with addProduct and products is added

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import AddProduct from '../../subcomponents/AddProducrt/AddProduct';
+import Products from '../../subcomponents/Products/Products';
 
 const Admin = () => {
+    const [subContent, setSubContent] = useState("addProduct");
     return (
         <div>
             <h1>Admin</h1>
+            <div className="row vh-100 bg-light">
+                <div className="col-4 d-flex flex-column justify-content-center align-items-center">
+                    <button className="btn btn-primary" onClick={() => setSubContent('products')}>Manage Products</button>
+                    <br />
+                    <button className="btn btn-primary" onClick={() => setSubContent('addProduct')}>Add Products</button>
+                </div>
+                <div className="col-8">
+                    {
+                        subContent === 'addProduct' ?
+                            (<AddProduct />)
+                            :
+                            (<Products />)
+                    }
+                </div>
+            </div>
         </div>
     );
 };
